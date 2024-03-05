12c6b1f182b69c5f226d6f3abe018998869e6898
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-03-05 23:13:56 +0300

    Implemented basic list rendering

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
-<template>
-  <div>
-    <NuxtWelcome />
+<script setup>
+const vacancies = await $fetch("/api/vacancies");
+</script>
+
+<template class="light">
+  <div class="m-7">
+    <!--    <NuxtWelcome />-->
+    <!--    <VacancyCard />-->
+    <ul>
+      <li v-for="vacancy in vacancies.items">
+        <VacancyCard :name="vacancy.name" />
+        <!--        <p>{{ vacancy.name }}</p>-->
+      </li>
+    </ul>
   </div>
 </template>
---
+++ components/VacancyCard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { Vacancy } from "~/server/api/vacancies";
+const props = defineProps<Vacancy>();
+</script>
+
+<template>
+  <UCard>
+    <template #header>
+      <Placeholder class="h-8" />
+      <p>{{ props.name }}</p>
+    </template>
+
+    <Placeholder class="h-32" />
+
+    <template #footer>
+      <Placeholder class="h-8" />
+    </template>
+  </UCard>
+</template>
