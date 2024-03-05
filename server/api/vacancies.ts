a83a426417dc5dfc66cc59f6ca88680e3c5741a8
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-03-05 23:00:26 +0300

    Add Vacancy type draft

--- server/api/vacancies.ts
+++ server/api/vacancies.ts
@@ -1,5 +1,15 @@
-export default async function defineEventHandler() {
-  return await $fetch(
+export type Vacancy = {
+  id: number;
+  premium: boolean;
+  name: string;
+  department: string;
+  has_test: boolean;
+  response_letter_required: boolean;
+};
+
+export default defineEventHandler(async (event) => {
+  const res: Vacancy[] = await $fetch(
     "https://api.hh.ru/vacancies?area=1051&professional_role=156&professional_role=160&professional_role=10&professional_role=12&professional_role=150&professional_role=25&professional_role=165&professional_role=34&professional_role=36&professional_role=73&professional_role=155&professional_role=96&professional_role=164&professional_role=104&professional_role=157&professional_role=107&professional_role=112&professional_role=113&professional_role=148&professional_role=114&professional_role=116&professional_role=121&professional_role=124&professional_role=125&professional_role=126",
   );
-}
+  return res;
+});
