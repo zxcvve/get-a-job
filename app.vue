59787da7078e198842a8bfb0b6bd482ef2f4fb2d
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-03-07 20:54:02 +0300

    Center the VacancyCard list

--- app.vue
+++ app.vue
@@ -4,7 +4,11 @@ const vacancies = await $fetch("/api/vacancies/all");
 
 <template class="light">
   <div class="m-7">
-    <div v-for="vacancy in vacancies" :key="vacancy.id">
+    <div
+      v-for="vacancy in vacancies"
+      :key="vacancy.id"
+      class="max-w-screen-lg m-auto"
+    >
       <VacancyCard :name="vacancy.name" />
     </div>
   </div>
