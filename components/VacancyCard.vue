f5e49289b53225fe6b1e34b334d4e39333dc1ab0
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-03-07 21:21:31 +0300

    Added conditional salary display

--- components/VacancyCard.vue
+++ components/VacancyCard.vue
@@ -1,19 +1,26 @@
 <script setup lang="ts">
 import type { Vacancy } from "~/types";
-const props = defineProps<Vacancy>();
+
+const props = defineProps<{ vacancy: Vacancy }>();
 </script>
 
 <template>
   <UCard>
     <template #header>
       <!--      <Placeholder class="h-8" />-->
-      <p>{{ props.name }}</p>
+      <p>{{ props.vacancy.name }}</p>
     </template>
 
     <!--    <Placeholder class="h-32" />-->
 
-    <template #footer>
-      <!--      <Placeholder class="h-8" />-->
+    <template v-if="props.vacancy.salary" #footer>
+      <p>
+        Зарплата от {{ props.vacancy.salary.from }}
+        <span v-if="props.vacancy.salary.to"
+          >до {{ props.vacancy.salary.to }}</span
+        >
+        {{ props.vacancy.salary.currency }}
+      </p>
     </template>
   </UCard>
 </template>
