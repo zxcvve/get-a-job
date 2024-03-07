05fd605e23477977450da0f4cfe336dc1c52ad80
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-03-07 21:22:12 +0300

    Added margin between cards, pass object as a prop

--- app.vue
+++ app.vue
@@ -2,14 +2,14 @@
 const vacancies = await $fetch("/api/vacancies/all");
 </script>
 
-<template class="light">
+<template>
   <div class="m-7">
     <div
       v-for="vacancy in vacancies"
       :key="vacancy.id"
       class="max-w-screen-lg m-auto"
     >
-      <VacancyCard :name="vacancy.name" />
+      <VacancyCard class="m-2" :vacancy="vacancy" />
     </div>
   </div>
 </template>
