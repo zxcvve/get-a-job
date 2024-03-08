dd7e4ca8124aab00de0590c8ae6cd1171eb22d26
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-03-08 22:21:53 +0300

    Installed nuxt-scheduler module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/eslint-module", "@nuxt/ui", "@nuxtjs/supabase"],
+  modules: [
+    "@nuxtjs/eslint-module",
+    "@nuxt/ui",
+    "@nuxtjs/supabase",
+    "nuxt-scheduler",
+  ],
   eslint: { lintOnStart: false },
   colorMode: { preference: "light" },
   supabase: {
