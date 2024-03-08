737a07b07e36e1c97f80c04035381c40387a5963
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-03-08 22:19:45 +0300

    Fixed variable name

--- server/api/supabase/vacancies.get.ts
+++ server/api/supabase/vacancies.get.ts
@@ -2,7 +2,7 @@ import { serverSupabaseClient } from "#supabase/server";
 
 export default eventHandler(async (event) => {
   const supabase = await serverSupabaseClient(event);
-  const { data: service, error } = await supabase.from("vacancy").select("*");
+  const { data: vacancies, error } = await supabase.from("vacancy").select("*");
 
-  return { services: service };
+  return { vacancies: vacancies };
 });
