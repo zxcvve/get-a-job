47a6828ec0df6e60bd75d61e9047aff5cccdd418
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-03-08 22:59:48 +0300

    Fetch vacancies from supabase DB instead of HeadHunter API

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <script setup>
-const vacancies = await $fetch("/api/vacancies/all");
+// const vacancies = await $fetch("/api/vacancies/all");
+const vacancies = await $fetch("/api/supabase/vacancies");
 </script>
 
 <template>
-  <!--TODO: fix styling-->
+  <!--TODO: fix styling, use nuxtui Container and Skeleton maybe?-->
   <div class="m-7">
     <div class="max-w-screen-lg m-auto space-y-1.5">
       <div v-for="vacancy in vacancies" :key="vacancy.id">
