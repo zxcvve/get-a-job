f1af948abf3ab944d21aecf9f0baccb344b53a53
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-03-08 22:57:28 +0300

    Fix returned type

--- server/api/supabase/vacancies.get.ts
+++ server/api/supabase/vacancies.get.ts
@@ -4,5 +4,5 @@ export default eventHandler(async (event) => {
   const supabase = await serverSupabaseClient(event);
   const { data: vacancies, error } = await supabase.from("vacancy").select("*");
 
-  return { vacancies: vacancies };
+  return vacancies;
 });
