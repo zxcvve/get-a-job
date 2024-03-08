74d3d2be65b6a99983cf0f8e48a92a9e622ff584
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-03-08 13:54:18 +0300

    Every card has a link to the vacancy now

--- components/VacancyCard.vue
+++ components/VacancyCard.vue
@@ -5,22 +5,24 @@ const props = defineProps<{ vacancy: Vacancy }>();
 </script>
 
 <template>
-  <UCard>
-    <template #header>
-      <!--      <Placeholder class="h-8" />-->
-      <p>{{ props.vacancy.name }}</p>
-    </template>
+  <NuxtLink :to="props.vacancy.alternate_url">
+    <UCard>
+      <template #header>
+        <!--      <Placeholder class="h-8" />-->
+        <p>{{ props.vacancy.name }}</p>
+      </template>
 
-    <!--    <Placeholder class="h-32" />-->
+      <!--    <Placeholder class="h-32" />-->
 
-    <template v-if="props.vacancy.salary" #footer>
-      <p>
-        Зарплата от {{ props.vacancy.salary.from }}
-        <span v-if="props.vacancy.salary.to"
-          >до {{ props.vacancy.salary.to }}</span
-        >
-        {{ props.vacancy.salary.currency }}
-      </p>
-    </template>
-  </UCard>
+      <template v-if="props.vacancy.salary" #footer>
+        <p>
+          Зарплата от {{ props.vacancy.salary.from }}
+          <span v-if="props.vacancy.salary.to"
+            >до {{ props.vacancy.salary.to }}</span
+          >
+          {{ props.vacancy.salary.currency }}
+        </p>
+      </template>
+    </UCard>
+  </NuxtLink>
 </template>
