00a0b303a446094cfd7bc61ecb0b8ac1d4c32131
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-03-08 13:53:12 +0300

    Styling changes

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,12 @@ const vacancies = await $fetch("/api/vacancies/all");
 </script>
 
 <template>
+  <!--TODO: fix styling-->
   <div class="m-7">
-    <div
-      v-for="vacancy in vacancies"
-      :key="vacancy.id"
-      class="max-w-screen-lg m-auto"
-    >
-      <VacancyCard class="m-2" :vacancy="vacancy" />
+    <div class="max-w-screen-lg m-auto space-y-1.5">
+      <div v-for="vacancy in vacancies" :key="vacancy.id">
+        <VacancyCard :vacancy="vacancy" />
+      </div>
     </div>
   </div>
 </template>
