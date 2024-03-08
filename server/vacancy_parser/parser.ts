4c1b7a4c537c4a188dc317f580c8da30b25f4816
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-03-08 22:20:09 +0300

    Vacancy parser draft

---
+++ server/vacancy_parser/parser.ts
@@ -0,0 +1,25 @@
+import { serverSupabaseClient } from "#supabase/server";
+
+async function addHHVacanciesToDB() {
+  const hhVacancies = await $fetch("api/vacancies/all");
+}
+
+export default eventHandler(async (event) => {
+  const supabase = await serverSupabaseClient(event);
+
+  const insertData = [
+    {
+      name: "Name",
+      url: "Url",
+      salary: "Salary",
+      employer: "Employer",
+      service: 1,
+    },
+  ];
+  // @ts-ignore
+  const { data, error } = await supabase
+    .from("vacancy")
+    .insert(insertData)
+    .select();
+  return data;
+});
