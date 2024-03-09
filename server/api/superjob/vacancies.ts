1804d3f9f11f4e51aa33008692f558230d17344b
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-03-09 15:35:29 +0300

    Add API route for fetching SuperJob vacancies

---
+++ server/api/superjob/vacancies.ts
@@ -0,0 +1,18 @@
+const SJ_VACANCIES =
+  "https://api.superjob.ru/2.0/vacancies?town=Великий Новгород&catalogues=33";
+
+// TODO: add correct type definition for superjob API responses
+export async function fetchVacancies() {
+  // @ts-ignore
+  return await $fetch(SJ_VACANCIES, {
+    headers: {
+      "X-Api-App-Id": process.env.SUPERJOB_KEY,
+    },
+  });
+}
+
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event);
+  // @ts-ignore
+  return fetchVacancies(query.page);
+});
