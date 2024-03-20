5ce5eeb67552a8491712f3c5e86822630026bfc0
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-03-20 21:12:08 +0300

    Implemented vacancies pagination API request

--- server/api/supabase/vacancies.get.ts
+++ server/api/supabase/vacancies.get.ts
@@ -1,8 +1,19 @@
 import { serverSupabaseClient } from "#supabase/server";
 
-export default eventHandler(async (event) => {
+async function fetchSupabaseVacancies(page = 1, event) {
   const supabase = await serverSupabaseClient(event);
-  const { data: vacancies, error } = await supabase.from("vacancy").select("*");
+  const pageMultiplier = 10 * (page - 1);
+  const lowerRange = pageMultiplier;
+  const upperRange = 9 + pageMultiplier;
+  const { data, error } = await supabase
+    .from("vacancy")
+    .select("*")
+    .range(lowerRange, upperRange);
+  return data;
+}
 
-  return vacancies;
+export default eventHandler(async (event) => {
+  const query = getQuery(event);
+  // @ts-ignore
+  return fetchSupabaseVacancies(query.page, event);
 });
