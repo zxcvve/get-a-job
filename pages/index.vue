0e32a2255f2b53c7993ff968480daa3d3118dcc5
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-05-04 23:35:29 +0300

    Add initial filter implementation

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,26 @@
 <script setup>
-// const vacancies = await $fetch("/api/vacancies/all");
-import VacancyFilter from "~/components/VacancyFilter.vue";
-
-const vacancies = await $fetch("/api/supabase/vacancies");
 import { NFlex } from "naive-ui";
+import vacancyFilter from "~/components/VacancyFilter.vue";
+
+const vacancies = useState("vacancies");
+await callOnce(async () => {
+  vacancies.value = await $fetch("/api/supabase/vacancies");
+});
+
+const filterVacancies = async (filter) => {
+  if (filter.value.selectedSalary) {
+    vacancies.value = await $fetch(
+      `/api/supabase/vacancies?salaryFrom=${filter.value.selectedSalary}`,
+    );
+  }
+};
+
+const resetVacancies = async (filter) => {
+  console.log(filter.value.selectedSalary);
+  if (filter.value.selectedSalary !== undefined) {
+    vacancies.value = await $fetch("/api/supabase/vacancies");
+  }
+};
 </script>
 
 <template>
@@ -11,7 +28,11 @@ import { NFlex } from "naive-ui";
     <!--TODO: fix styling, use nuxtui Container and Skeleton maybe?-->
     <div class="flex justify-center">
       <NFlex vertical class="max-w-screen-md">
-        <VacancyFilter />
+        <VacancyFilter
+          v-model:vacancyFilter="vacancyFilter"
+          @filter-clicked="filterVacancies"
+          @reset-clicked="resetVacancies"
+        />
         <div v-for="vacancy in vacancies" :key="vacancy.id" class="m-1">
           <VacancyCard :vacancy="vacancy" />
         </div>
