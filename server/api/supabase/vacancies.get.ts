939c09c44aec8b13c12078c1a104d8b152d2799a
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-05-04 16:17:31 +0300

    Implement "salaryFrom" parameter for vacancies.get.ts route

--- server/api/supabase/vacancies.get.ts
+++ server/api/supabase/vacancies.get.ts
@@ -1,10 +1,27 @@
 import { serverSupabaseClient } from "#supabase/server";
 
-async function fetchSupabaseVacancies(page = 1, event) {
+async function fetchSupabaseVacancies(page = 1, salaryFrom?, schedule?, event) {
   const supabase = await serverSupabaseClient(event);
   const pageMultiplier = 10 * (page - 1);
   const lowerRange = pageMultiplier;
   const upperRange = 9 + pageMultiplier;
+  if (salaryFrom) {
+    const { data, error } = await supabase
+      .from("vacancy")
+      .select(
+        `
+        id,
+        name,
+        url,
+        employer,
+        service,
+        minimal_salary: salary->from
+      `,
+      )
+      .gte("salary->from", salaryFrom)
+      .range(lowerRange, upperRange);
+    return data;
+  }
   const { data, error } = await supabase
     .from("vacancy")
     .select("*")
@@ -15,5 +32,10 @@ async function fetchSupabaseVacancies(page = 1, event) {
 export default eventHandler(async (event) => {
   const query = getQuery(event);
   // @ts-ignore
-  return fetchSupabaseVacancies(query.page, event);
+  return fetchSupabaseVacancies(
+    query.page,
+    query.salaryFrom,
+    query.schedule,
+    event,
+  );
 });
