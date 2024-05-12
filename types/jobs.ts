55dba76d20c448edbf7384a3df074b731bcea916
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-05-12 15:24:02 +0300

    Add SuperJob specific types

--- types/jobs.ts
+++ types/jobs.ts
@@ -61,3 +61,112 @@ export type HHVacanciesResponse = {
 };
 
 export type LogoUrls = { 90: string; original: string; 240: string };
+
+export type SuperJobVacanciesResponse = {
+  objects: SuperJobVacancy[];
+  total: number;
+  more: boolean;
+  subscription_id: number;
+  subscription_active: boolean;
+};
+
+export type SuperJobVacancy = {
+  canEdit: boolean;
+  is_closed: boolean;
+  id: number;
+  id_client: number;
+  payment_from: number;
+  payment_to: number;
+  date_oub_to: number;
+  date_archived: number;
+  date_published: number;
+  address: string;
+  profession: string;
+  work: null;
+  compensation: null;
+  candidat: string;
+  metro: null[];
+  currency: string;
+  vacancyRichText: string;
+  covid_vaccination_requirement: {
+    id: number;
+    title: string;
+  };
+  moveable: boolean;
+  agreement: boolean;
+  anonymous: boolean;
+  is_archive: boolean;
+  is_storage: boolean;
+  type_of_work: {
+    id: number;
+    title: string;
+  };
+  place_of_work: {
+    id: number;
+    title: string;
+  };
+  education: {
+    id: number;
+    title: string;
+  };
+  experience: {
+    id: number;
+    title: string;
+  };
+  maritalstatus: {
+    id: number;
+    title: string;
+  };
+  children: {
+    id: number;
+    title: string;
+  };
+  client: {
+    id: number;
+    title: string;
+    link: string;
+    industry: [];
+    description: string;
+    vacancy_count: number;
+    staff_count: string;
+    client_logo: string;
+    address: string | null;
+    addresses: string[] | [];
+    url: string;
+    short_reg: boolean;
+    is_blocked: boolean;
+    registered_date: number;
+    town: {
+      id: number;
+      title: string;
+      declension: string;
+      hasMetro: boolean;
+      genitive: string;
+    };
+  };
+  languages: [];
+  driving_license: [];
+  agency: {
+    id: number;
+    title: string;
+  };
+  town: {
+    id: number;
+    title: string;
+    declension: string;
+    hasMetro: boolean;
+    genitive: string;
+  };
+  phone: string;
+  phones: null;
+  client_logo: string;
+  age_from: number;
+  age_to: number;
+  gender: {
+    id: number;
+    title: string;
+  };
+  firm_name: string;
+  firm_activity: string;
+  link: string;
+};
