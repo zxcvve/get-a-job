0353a84a3f3cdd1b32e7ee446cf1ae85809177b5
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-05-12 14:54:52 +0300

    Fix eslint errors

--- server/api/supabase/services.get.ts
+++ server/api/supabase/services.get.ts
@@ -2,7 +2,7 @@ import { serverSupabaseClient } from "#supabase/server";
 
 export default eventHandler(async (event) => {
   const supabase = await serverSupabaseClient(event);
-  const { data: service, error } = await supabase.from("service").select("*");
+  const { data: service } = await supabase.from("service").select("*");
 
   return { services: service };
 });
