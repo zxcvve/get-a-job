1f8e12002292d23f6c7ae474a1d179b164e37a54
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-05-13 20:18:53 +0300

    Add ability to pass page number to superjob vacancies API route

--- server/api/superjob/vacancies.ts
+++ server/api/superjob/vacancies.ts
@@ -3,8 +3,10 @@ import type { SuperJobVacanciesResponse } from "~/types/jobs";
 const SJ_VACANCIES =
   "https://api.superjob.ru/2.0/vacancies?town=Великий Новгород&catalogues=33";
 
-export async function fetchVacancies(): Promise<SuperJobVacanciesResponse> {
-  return await $fetch(SJ_VACANCIES, {
+export async function fetchVacancies(
+  page = 0,
+): Promise<SuperJobVacanciesResponse> {
+  return await $fetch(SJ_VACANCIES + `&page=${page}`, {
     headers: {
       "X-Api-App-Id": process.env.SUPERJOB_KEY!,
     },
