bfc03f8d2e06efa5c499db0c40bcc602e618b080
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-05-13 20:26:27 +0300

    Add api route for fetching all superjob vacancies

---
+++ server/api/superjob/all.ts
@@ -0,0 +1,23 @@
+import { fetchVacancies } from "~/server/api/superjob/vacancies";
+import type { SuperJobVacancy } from "~/types/jobs";
+
+export async function fetchAllHHVacancies() {
+  const res = await fetchVacancies();
+
+  const vacancies: SuperJobVacancy[] = [];
+  let page = 0;
+
+  do {
+    const tempRes = await fetchVacancies(page);
+    page += 1;
+    tempRes.objects.map((vacancy) => {
+      vacancies.push(vacancy);
+    });
+  } while (res.more);
+
+  return vacancies;
+}
+
+export default defineEventHandler(async () => {
+  return fetchAllHHVacancies();
+});
