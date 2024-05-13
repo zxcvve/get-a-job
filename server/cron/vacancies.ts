f0f5d3327528970ea3f0f0d3ef4440517af07241
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-05-13 21:29:08 +0300

    Add function to fetch and add superjob vacancies to db

--- server/cron/vacancies.ts
+++ server/cron/vacancies.ts
@@ -1,8 +1,16 @@
 import { fetchAllHHVacancies } from "~/server/api/hh/all";
-import type { HHVacancy, SupabaseVacancy, SuperJobVacancy } from "~/types/jobs";
+import type {
+  Experience,
+  HHVacancy,
+  Salary,
+  Schedule,
+  SupabaseVacancy,
+  SuperJobVacancy,
+} from "~/types/jobs";
 import { defineCronHandler } from "#nuxt/cron";
 import type { SupabaseClient } from "@supabase/supabase-js";
 import { createClient } from "@supabase/supabase-js";
+import { fetchAllSuperJobVacancies } from "~/server/api/superjob/all";
 
 async function deleteOldVacancies(supabase: SupabaseClient, tableName: string) {
   // time delta calculations for removing vacancies older than 24 hours
@@ -16,6 +24,66 @@ async function deleteOldVacancies(supabase: SupabaseClient, tableName: string) {
     .lt("created_at", timeDelta)
     .select();
   return { data, error };
+}
+
+async function collectSuperJobVacancies() {
+  const superJobVacancies: SuperJobVacancy[] =
+    await fetchAllSuperJobVacancies();
+  const processedSuperJobVacancies: SupabaseVacancy[] = [];
+  superJobVacancies.map((vacancy) => {
+    const vacancySchedule: Schedule = { id: "", name: "" };
+    switch (vacancy.place_of_work.id) {
+      case 2:
+        vacancySchedule.id = "remote";
+        vacancySchedule.name = "Удаленная работа";
+        break;
+      case 1:
+      default:
+        vacancySchedule.id = "fullDay";
+        vacancySchedule.name = "Полный день";
+        break;
+    }
+
+    const vacancyExperience: Experience = { id: "", name: "" };
+    switch (vacancy.experience.id) {
+      case 2:
+        vacancyExperience.id = "between1And3";
+        vacancyExperience.name = "От 1 года до 3 лет";
+        break;
+      case 3:
+        vacancyExperience.id = "between1And6";
+        vacancyExperience.name = "От 3 до 6 лет";
+        break;
+      case 4:
+        vacancyExperience.id = "moreThan6";
+        vacancyExperience.name = "Более 6 лет";
+        break;
+      case 1:
+      default:
+        vacancyExperience.id = "noExperience";
+        vacancyExperience.name = "Нет опыта";
+        break;
+    }
+
+    const vacancySalary: Salary = {
+      from: vacancy.payment_from,
+      to: vacancy.payment_to,
+      currency: "RUR",
+      gross: false,
+    };
+
+    const insertData: SupabaseVacancy = {
+      name: vacancy.profession,
+      salary: vacancySalary,
+      url: vacancy.link,
+      employer: vacancy.client.title,
+      schedule: vacancySchedule,
+      experience: vacancyExperience,
+      service: 2,
+    };
+    processedSuperJobVacancies.push(insertData);
+  });
+  return processedSuperJobVacancies;
 }
 
 async function collectHHVacancies() {
@@ -50,6 +118,9 @@ async function addHHVacanciesToDB(supabase: SupabaseClient) {
   if (res?.data?.length > 0) {
     const HHVacancies = await collectHHVacancies();
     processedVacancies.concat(HHVacancies);
+
+    const superJobVacancies = await collectSuperJobVacancies();
+    processedVacancies.concat(superJobVacancies);
   }
 
   const { error } = await supabase.from("vacancy").insert(processedVacancies);
