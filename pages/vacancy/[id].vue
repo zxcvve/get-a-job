175afb17b62e577ee7eeafc75cb33674915a9d39
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-05-18 18:57:36 +0300

    Add vacancy page

---
+++ pages/vacancy/[id].vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const route = useRoute();
+
+const vacancy = await $fetch(`/api/supabase/${route.params.id}`);
+</script>
+
+<template>
+  <naive-config>
+    <div class="flex justify-center">
+      <div class="w-96 m-4 md:w-1/3">{{ vacancy.id }}</div>
+    </div></naive-config
+  >
+</template>
