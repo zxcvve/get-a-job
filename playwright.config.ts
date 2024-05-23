1145b76030de835fef0af7cbb3a1c553ff0100b7
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-05-24 00:53:33 +0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,6 +72,6 @@ export default defineConfig({
   webServer: {
     command: "npm run dev",
     url: "http://127.0.0.1:3000",
-    reuseExistingServer: true,
+    reuseExistingServer: !process.env.CI,
   },
 });
